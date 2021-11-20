c4e7ba0fa83635880a9cfe146666054ae9fda642
Naor Beeri <nnaaoorr0022@gmail.com>
2021-11-20 19:07:16 +0200

    all done

--- tictactoeUI.js
+++ tictactoeUI.js
@@ -5,25 +5,23 @@ enter code to handle:
 3. Start button click
 */
 window.onload = function () {
-    const boardArray = new Array(9);
+    let boardArray = new Array(9);
     const buttonArray = new Array(9);
     for (let i = 0; i < buttonArray.length; i++) {
         buttonArray[i] = document.getElementById("b".concat(i.toString()));
-        
     }
     disableButtons(buttonArray);
     var turn;
     const statusFooter = document.getElementById("gameStatusTag");
-    statusFooter.innerHTML="Press Start to begin";
+    statusFooter.innerHTML = "Press Start to begin";
     function restart() {
         for (let i = 0; i < buttonArray.length; i++) {
-            buttonArray[i].innerHTML='';
+            buttonArray[i].innerHTML = '';
             enableButtons(buttonArray);
         }
         boardArray = new Array(9);
         turn = "x";
     }
-    var should_click = false;
     document.querySelector("header>button").onclick = function () {
         if (document.querySelector("header>button").innerHTML === "Restart") {
             restart();
@@ -33,31 +31,10 @@ window.onload = function () {
             turn = "x";
             enableButtons(buttonArray);
             for (let i = 0; i < buttonArray.length; i++) {
-                buttonArray[i].addEventListener('click', clickhandler(i)); // runs this 
-            }
-            should_click = true;
-        }
-        statusFooter.innerHTML=`${turn}'s turn`;
-    }
-    function clickhandler(button_num) {
-        if(should_click) {
-            if(buttonArray[button_num].innerHTML == '') {
-                buttonArray[button_num].innerHTML = turn;
-                boardArray[button_num] = turn;
-                if (hasWon(boardArray)) {
-                    disableButtons(buttonArray);
-                    statusFooter.innerHTML=`${turn} wins! Hit RESTART to start a new game`;
-                }
-                else if(hasDraw(boardArray)) {
-                    statusFooter.innerHTML=`Draw! You both suck!`;
-                }
-                if(turn === "x")
-                    turn = "o";
-                else
-                    turn = "x";
-                statusFooter.innerHTML=`${turn}'s turn`;
+                clickhandler(buttonArray[i], i); // runs this 
             }
         }
+        statusFooter.innerHTML = `${turn}'s turn`;
     }
     function disableButtons(array) {
         for (let i = 0; i < array.length; i++) {
@@ -68,5 +45,28 @@ window.onload = function () {
         for (let i = 0; i < array.length; i++) {
             array[i].disabled = false;
         }
+    }
+    function clickhandler(elem, i) {
+        elem.addEventListener('click', function () {
+            if (buttonArray[i].innerHTML == '') {
+                buttonArray[i].innerHTML = turn;
+                boardArray[i] = turn;
+                if (hasWon(boardArray)) {
+                    disableButtons(buttonArray);
+                    statusFooter.innerHTML = `${turn} wins! Hit RESTART to start a new game`;
+                }
+                else if (hasDraw(boardArray)) {
+                    disableButtons(buttonArray);
+                    statusFooter.innerHTML = `Draw! You both suck!`;
+                }
+                else {
+                    if (turn === "x")
+                        turn = "o";
+                    else
+                        turn = "x";
+                    statusFooter.innerHTML = `${turn}'s turn`;
+                }
+            }
+        }, false);
     }
 }
