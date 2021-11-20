1d56708e40d320d614ca797a95827ccf46fccbf5
Naor Beeri <nnaaoorr0022@gmail.com>
2021-11-20 18:38:42 +0200

    123

--- tictactoe.js
+++ tictactoe.js
@@ -11,16 +11,40 @@
 ////Diagonally1['x',' ',' ',' ','x',' ',' ',' ','x'];
 ////Diagonally2[' ',' ','x',' ','x',' ','x',' ',' '];
 //
-//recieve a string array parameter and return true
-//if the game is a draw or false if not.
-function hasDraw(selectionArray) {
-   return false;
-}
 //recieve a string array parameter and return true 
 //if someone won the game or false if not.
 function hasWon(selectionArray) {
+    for (let i = 0; i < 3; i++) {
+        if ((selectionArray[i] === selectionArray[i + 3]) && (selectionArray[i] === selectionArray[i + 6]) && (selectionArray[i] !== undefined) && (selectionArray[i] !== " "))
+            return true; // horizontal
+    }
+    for (let i = 0; i < 7; i += 3) {
+        if ((selectionArray[i] === selectionArray[i + 1]) && (selectionArray[i] === selectionArray[i + 2]) && (selectionArray[i] !== undefined) && (selectionArray[i] !== " "))
+            return true; // vertical
+    }
+    if ((selectionArray[0] === selectionArray[4]) && (selectionArray[0] === selectionArray[8]) && (selectionArray[0] !== undefined) && (selectionArray[0] !== " ")) {
+        return true;
+    }
+    if ((selectionArray[2] === selectionArray[4]) && (selectionArray[2] === selectionArray[6]) && (selectionArray[2] !== undefined) && (selectionArray[2] !== " ")) {
+        return true;
+    }
     return false;
 }
+
+
+//recieve a string array parameter and return true
+//if the game is a draw or false if not.
+function hasDraw(selectionArray) {
+    for (let i=0; i<9; i++) {
+        if ((selectionArray[i] === undefined) || (selectionArray[i] === " ")) {
+            return false;
+        }
+    }
+    return !hasWon(selectionArray);
+}
+//recieve a string array parameter and return true 
+//if someone won the game or false if not.
+
 //Please , don't modify the following code
 if (typeof module !== "undefined") {
     module.exports = {
--- tictactoeUI.js
+++ tictactoeUI.js
@@ -1,6 +1,72 @@
 /*
 enter code to handle:
-1. User cell selection click
+1. User cell selection click 
 2. game status update messages
 3. Start button click
 */
+window.onload = function () {
+    const boardArray = new Array(9);
+    const buttonArray = new Array(9);
+    for (let i = 0; i < buttonArray.length; i++) {
+        buttonArray[i] = document.getElementById("b".concat(i.toString()));
+        
+    }
+    disableButtons(buttonArray);
+    var turn;
+    const statusFooter = document.getElementById("gameStatusTag");
+    statusFooter.innerHTML="Press Start to begin";
+    function restart() {
+        for (let i = 0; i < buttonArray.length; i++) {
+            buttonArray[i].innerHTML='';
+            enableButtons(buttonArray);
+        }
+        boardArray = new Array(9);
+        turn = "x";
+    }
+    var should_click = false;
+    document.querySelector("header>button").onclick = function () {
+        if (document.querySelector("header>button").innerHTML === "Restart") {
+            restart();
+        }
+        else {
+            document.querySelector("header>button").innerHTML = "Restart";
+            turn = "x";
+            enableButtons(buttonArray);
+            for (let i = 0; i < buttonArray.length; i++) {
+                buttonArray[i].addEventListener('click', clickhandler(i)); // runs this 
+            }
+            should_click = true;
+        }
+        statusFooter.innerHTML=`${turn}'s turn`;
+    }
+    function clickhandler(button_num) {
+        if(should_click) {
+            if(buttonArray[button_num].innerHTML == '') {
+                buttonArray[button_num].innerHTML = turn;
+                boardArray[button_num] = turn;
+                if (hasWon(boardArray)) {
+                    disableButtons(buttonArray);
+                    statusFooter.innerHTML=`${turn} wins! Hit RESTART to start a new game`;
+                }
+                else if(hasDraw(boardArray)) {
+                    statusFooter.innerHTML=`Draw! You both suck!`;
+                }
+                if(turn === "x")
+                    turn = "o";
+                else
+                    turn = "x";
+                statusFooter.innerHTML=`${turn}'s turn`;
+            }
+        }
+    }
+    function disableButtons(array) {
+        for (let i = 0; i < array.length; i++) {
+            array[i].disabled = true;
+        }
+    }
+    function enableButtons(array) {
+        for (let i = 0; i < array.length; i++) {
+            array[i].disabled = false;
+        }
+    }
+}
